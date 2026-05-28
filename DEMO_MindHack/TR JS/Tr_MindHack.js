38669054d4f17d2597d08d7b610f0e90b81149f4
TruongKhoaNguyen <huynhbichtran050185@gmail.com>
2026-05-29 00:26:31 +0700

    modified:   DEMO_MindHack/KH CSS/Kh_sign_up.css 	modified:   DEMO_MindHack/MH HTML/MH_signup.html 	modified:   DEMO_MindHack/MH HTML/MindHack.html 	new file:   DEMO_MindHack/TR JS/Tr_MindHack.js 	new file:   DEMO_MindHack/TR JS/Tr_main_navbar.js 	new file:   DEMO_MindHack/images/Google_su_icon.png

---
+++ DEMO_MindHack/TR JS/Tr_MindHack.js
@@ -0,0 +1,9 @@
+const StartTrialButton = document.querySelector("#Tr-start-trial");
+const ForDevButton = document.querySelector("#Tr-for-dev");
+
+StartTrialButton.onclick = () => {
+    location.href = "../MH HTML/MH_signup.html";
+}
+ForDevButton.onclick = () => {
+    location.href = "../MH HTML/MH_construct.html";
+}
---
+++ DEMO_MindHack/TR JS/Tr_main_navbar.js
@@ -0,0 +1,13 @@
+const LogInButton = document.querySelector("#Tr-login-button");
+const RequestDemoButton = document.querySelector("#Tr-request-button");
+const SignUpButton = document.querySelector("#Tr-signup-button");
+
+LogInButton.onclick = () => {
+    location.href = "../MH HTML/MH_construct.html";
+}
+RequestDemoButton.onclick = () => {
+    location.href = "../MH HTML/MH_construct.html";
+}
+SignUpButton.onclick = () => {
+    location.href = "../MH HTML/MH_signup.html";
+}
